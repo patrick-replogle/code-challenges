e291b340a6d0bdc6b97f10786d8a8e40ad06ae23
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-10-21 20:57:24 -0700

    Create isBeautifulString.js

---
+++ codesignal/isBeautifulString.js
@@ -0,0 +1,43 @@
+// A string is said to be beautiful if each letter in the string appears at most as many times as the previous letter in the alphabet within the string; 
+// ie: b occurs no more times than a; c occurs no more times than b; etc.
+
+// Given a string, check whether it is beautiful.
+
+// Example
+
+// For inputString = "bbbaacdafe", the output should be isBeautifulString(inputString) = true.
+
+// This string contains 3 as, 3 bs, 1 c, 1 d, 1 e, and 1 f (and 0 of every other letter), so since there aren't any letters that appear more frequently 
+// than the previous letter, this string qualifies as beautiful.
+
+// For inputString = "aabbb", the output should be isBeautifulString(inputString) = false.
+
+// Since there are more bs than as, this string is not beautiful.
+
+// For inputString = "bbc", the output should be isBeautifulString(inputString) = false.
+
+// Although there are more bs than cs, this string is not beautiful because there are no as, so therefore there are more bs than as.
+
+function isBeautifulString(inputString) {
+    let charCount = {"a": 0};
+    
+    for (let i = 1; i < 27; i++) {
+        charCount[String.fromCharCode(("a".charCodeAt(0) + i))] = 0;
+    }
+    
+    for (let char of inputString) {
+        if (!charCount.hasOwnProperty(char)) {
+            charCount[char] = 0;
+        }
+        charCount[char] += 1;
+    }
+
+    for (let key in charCount) {
+        let prevChar = String.fromCharCode((key.charCodeAt(0) - 1));
+        
+        if (charCount[prevChar] < charCount[key]) {
+            return false;
+        }
+    }
+    return true;
+}
