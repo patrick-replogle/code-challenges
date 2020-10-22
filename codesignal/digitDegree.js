6433ee5070f38e8e4e409a8d589aab16846cfe66
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-10-21 18:16:58 -0700

    Create digitDegree.js

---
+++ codesignal/digitDegree.js
@@ -0,0 +1,36 @@
+// Let's define digit degree of some positive integer as the number of times we need to replace this number with the sum of its digits until we 
+// get to a one digit number.
+
+// Given an integer, find its digit degree.
+
+// Example
+
+// For n = 5, the output should be
+// digitDegree(n) = 0;
+// For n = 100, the output should be
+// digitDegree(n) = 1.
+// 1 + 0 + 0 = 1.
+// For n = 91, the output should be
+// digitDegree(n) = 2.
+// 9 + 1 = 10 -> 1 + 0 = 1.
+
+function digitDegree(n) {
+    if (n <= 9) {
+        return 0;
+    }
+    
+    let count = 0;
+    
+    while (n > 9) {
+        let arr = String(n).split("");
+        let curr = 0;
+        
+        arr.forEach((num) => {
+            curr += Number(num);
+        })
+        
+        count++;
+        n = curr;
+    }
+    return count;
+}
