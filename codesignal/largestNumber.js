b4c020751c75a1f05e29cf9b07b7946f219065ae
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-10-26 19:38:10 -0700

    Create largestNumber.js

---
+++ codesignal/largestNumber.js
@@ -0,0 +1,10 @@
+// Given an integer n, return the largest number that contains exactly n digits.
+
+// Example
+
+// For n = 2, the output should be
+// largestNumber(n) = 99.
+
+function largestNumber(n) {
+    return Number("9".repeat(n));
+}
