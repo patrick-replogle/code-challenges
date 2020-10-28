9e4c7188d792b3fd9921589dc994376b34914263
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-10-27 19:32:38 -0700

    Create countSumOfTwoRepresentations2.js

---
+++ codesignal/countSumOfTwoRepresentations2.js
@@ -0,0 +1,20 @@
+// Given integers n, l and r, find the number of ways to represent n as a sum of two integers A and B such that l ≤ A ≤ B ≤ r.
+
+// Example
+
+// For n = 6, l = 2, and r = 4, the output should be
+// countSumOfTwoRepresentations2(n, l, r) = 2.
+
+// There are just two ways to write 6 as A + B, where 2 ≤ A ≤ B ≤ 4: 6 = 2 + 4 and 6 = 3 + 3.
+
+function countSumOfTwoRepresentations2(n, l, r) {
+    let count = 0;
+    
+    for (let i = l; i <= r; i++) {
+        for (let j = l; j <= r; j++) {
+            if (j > i || j + i > n) break
+            else if (i + j === n) count++
+        }
+    }
+    return count;
+}
