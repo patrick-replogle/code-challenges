58e94ece81a4133f8a028be8ddb1bf0aa47a1f57
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-10-28 15:27:27 -0700

    Create createArray.js

---
+++ codesignal/createArray.js
@@ -0,0 +1,10 @@
+// Given an integer size, return array of length size filled with 1s.
+
+// Example
+
+// For size = 4, the output should be
+// createArray(size) = [1, 1, 1, 1].
+
+function createArray(size) {
+    return new Array(size).fill(1);
+}
