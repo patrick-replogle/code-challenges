6f094bc9e4676467a707ba6dc58f772b84f26de0
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-10-29 16:52:04 -0700

    Create stringsConstruction.js

---
+++ codesignal/stringsConstruction.js
@@ -0,0 +1,40 @@
+// Given two strings a and b, both consisting only of lowercase English letters, your task is to calculate how many strings equal to a can 
+// be constructed using only letters from the string b? Each letter can be used only once and in one string only.
+
+// Example
+
+// For a = "abc" and b = "abccba", the output should be stringsConstruction(a, b) = 2.
+
+// We can construct 2 strings a = "abc" using only letters from the string b.
+
+// For a = "ab" and b = "abcbcb", the output should be stringsConstruction(a, b) = 1.
+
+function stringsConstruction(a, b) {
+    let stillValidLettersLeft = true;
+    let count = 0;
+    let seen = {};
+    a = a.split("").sort();
+    
+    for (let char of b) {
+        if (!seen.hasOwnProperty(char)) {
+            seen[char] = 0;
+        }
+        seen[char] += 1;
+    }
+    
+    while (stillValidLettersLeft) {
+        let curr = "";
+        
+        for (let i = 0; i < a.length; i++) {
+            if (seen.hasOwnProperty(a[i]) && seen[a[i]] > 0) {
+                curr += a[i];
+                seen[a[i]] -= 1;
+            } else {
+                stillValidLettersLeft = false;
+                break;
+            }
+        }
+        if (curr.split("").sort().join("") === a.join("")) count++;
+    }
+    return count;
+}
