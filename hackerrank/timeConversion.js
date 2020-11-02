fea806f245f0c742f24147d786f70d81912de91d
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-11-01 16:26:15 -0800

    Update timeConversion.js

--- hackerrank/timeConversion.js
+++ hackerrank/timeConversion.js
@@ -1,7 +1,7 @@
-Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+// Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
 
-Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
-- 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+// Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+// - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
 
 function timeConversion(s) {
     let timeOfDay = s.substring(s.length - 2, s.length);
