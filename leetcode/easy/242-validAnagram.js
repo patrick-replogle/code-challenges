900d6332bebd710666a96f4c2265ab14e60e2700
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-11-17 12:15:04 -0800

    Update 242-validAnagram.js

--- leetcode/easy/242-validAnagram.js
+++ leetcode/easy/242-validAnagram.js
@@ -35,3 +35,16 @@ var isAnagram = function(s, t) {
     }
     return true;
 };
+
+// second pass solution
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+    
+    s = s.split("").sort();
+    t = t.split("").sort();
+    
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] !== t[i]) return false;
+    }
+    return true;
+}
