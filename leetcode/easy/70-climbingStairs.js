2179664fa8a65285904232759b334ea660307a80
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-11-16 20:17:15 -0800

    Update 70-climbingStairs.js

--- leetcode/easy/70-climbingStairs.js
+++ leetcode/easy/70-climbingStairs.js
@@ -66,8 +66,7 @@ var climbStairs = function(n) {
     dp[1] = 2;
     
     for (let i = 2; i < n; i++) {
-        dp[i] += dp[i -1];
-        dp[i] += dp[i - 2];
+        dp[i] += dp[i -1] + dp[i - 2];
     }
     return dp[n - 1];
 };
