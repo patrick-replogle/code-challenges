32cdaf22c4095a56e32272cc20211f72f44e0ecd
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-11-21 12:07:57 -0800

    Update 79-wordSearch.js

--- leetcode/medium/79-wordSearch.js
+++ leetcode/medium/79-wordSearch.js
@@ -15,7 +15,7 @@
 const exist = function(board, word) {     
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
-            if (backtrack(board, word, i, j)) {
+            if (backtrack(board, word, i, j)) { 
                 return true;
             }
         }
@@ -24,21 +24,20 @@ const exist = function(board, word) {
 }
 
 const backtrack = function(board, word, row, col) {
-    if (word.length === 0) return true;
+    if (word.length === 0) return true; // base case for a match
     if (row < 0 || row >= board.length) return false;
     if (col < 0 || col >= board[0].length) return false;
     
-    if (board[row][col] === word[0]) {
-        board[row][col] = "#";
-        if (
-            backtrack(board, word.slice(1), row + 1, col) ||
-            backtrack(board, word.slice(1), row - 1, col) ||
-            backtrack(board, word.slice(1), row, col + 1) ||
-            backtrack(board, word.slice(1), row, col - 1)
-        ) {
-          return true;  
-        }
-        board[row][col] = word[0];
+    if (board[row][col] === word[0]) { 
+        board[row][col] = "#"; // avoid using same position on the board more than once
+        
+        let down = backtrack(board, word.slice(1), row + 1, col) 
+        let up = backtrack(board, word.slice(1), row - 1, col) 
+        let right = backtrack(board, word.slice(1), row, col + 1) 
+        let left = backtrack(board, word.slice(1), row, col - 1)
+        
+        if (down || up || right || left) return true; // we found a match
+        board[row][col] = word[0]; // backtrack and reset the position on board back to original state
     }
     return false;
 }
