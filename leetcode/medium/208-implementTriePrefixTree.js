9a87f5cd823776f1bc591f77cdc9e1069d0f06ac
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-11-23 11:18:21 -0800

    Create 208-implementTriePrefixTree.js

---
+++ leetcode/medium/208-implementTriePrefixTree.js
@@ -0,0 +1,42 @@
+// Implement a trie with insert, search, and startsWith methods.
+
+// Example:
+
+// Trie trie = new Trie();
+
+// trie.insert("apple");
+// trie.search("apple");   // returns true
+// trie.search("app");     // returns false
+// trie.startsWith("app"); // returns true
+// trie.insert("app");   
+// trie.search("app");     // returns true
+
+// first pass solution using an ES6 class and set for data storage
+class Trie {
+    constructor() {
+        this.trie = new Set();
+    }
+    
+    insert(word) {
+        if (!(this.trie.has(word))) {
+            this.trie.add(word);
+        }
+    }
+    
+    search(word) {
+        if (this.trie.has(word)) {
+            return true;
+        }
+        return false;
+    }
+    
+    startsWith(prefix) {
+        for (let key of this.trie) {
+            if (key.startsWith(prefix)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
