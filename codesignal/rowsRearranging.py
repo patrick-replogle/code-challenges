59eb9e3a4f83bd5775595e1bade8ff0f8b019f81
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-11-24 20:42:20 -0800

    Update rowsRearranging.py

--- codesignal/rowsRearranging.py
+++ codesignal/rowsRearranging.py
@@ -1,25 +1,25 @@
-// Given a rectangular matrix of integers, check if it is possible to rearrange its rows in such a way that all its columns 
-// become strictly increasing sequences (read from top to bottom).
+# Given a rectangular matrix of integers, check if it is possible to rearrange its rows in such a way that all its columns 
+# become strictly increasing sequences (read from top to bottom).
 
-// Example
+# Example
 
-// For
+# For
 
-// matrix = [[2, 7, 1], 
-//           [0, 2, 0], 
-//           [1, 3, 1]]
-// the output should be
-// rowsRearranging(matrix) = false;
+# matrix = [[2, 7, 1], 
+#           [0, 2, 0], 
+#           [1, 3, 1]]
+# the output should be
+# rowsRearranging(matrix) = false;
 
-// For
+# For
 
-// matrix = [[6, 4], 
-//           [2, 2], 
-//           [4, 3]]
+# matrix = [[6, 4], 
+#           [2, 2], 
+#           [4, 3]]
 
-// first pass solution:
-// sort rows in asc order by first value in each row, then iteratite thru values in each column and return false if numbers aren't 
-// ascending from top row to bottom row
+# first pass solution:
+# sort rows in asc order by first value in each row, then iteratite thru values in each column and return false if numbers aren't 
+# ascending from top row to bottom row
 
 def rowsRearranging(matrix):
     matrix.sort(key=lambda x: x[0])
