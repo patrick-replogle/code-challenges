9b3d437cc1aa3a9640e86ad30a4cea5039667004
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-12-07 14:08:19 -0800

    Update 500-keyboardRow.js

--- leetcode/easy/500-keyboardRow.js
+++ leetcode/easy/500-keyboardRow.js
@@ -20,3 +20,29 @@ var findWords = function(words) {
     }
     return results;
 };
+
+// second pass solution using sets and a nested loop to count chars
+var findWords = function(words) {
+    let top = new Set(["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]);
+    let middle = new Set(["a", "s", "d", "f", "g", "h", "j", "k", "l"]);
+    let bottom = new Set(["z", "x", "c", "v", "b", "n", "m"]);
+    let results = [];
+    
+    for (let word of words) {
+        curr = word.toLowerCase();
+        let tLen = 0;
+        let mLen = 0;
+        let bLen = 0;
+        
+        for (let char of curr) {
+            if (top.has(char)) tLen++;
+            if (middle.has(char)) mLen++;
+            if (bottom.has(char)) bLen++;
+        }
+        
+        if (tLen === word.length || mLen === word.length || bLen === word.length) {
+            results.push(word);
+        }
+    }
+    return results;
+};
