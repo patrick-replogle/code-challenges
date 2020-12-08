83617fc5b455dcc53c39fe08a3a908e777b7b56b
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-12-08 13:49:10 -0800

    Create 744-findSmallestLetterGreaterThanTarget.js

---
+++ leetcode/easy/744-findSmallestLetterGreaterThanTarget.js
@@ -0,0 +1,35 @@
+// Given a list of sorted characters letters containing only lowercase letters, and given a target letter target, find the smallest element in the list that is larger than the given target.
+
+// Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
+
+// Examples:
+// Input:
+// letters = ["c", "f", "j"]
+// target = "a"
+// Output: "c"
+
+// Input:
+// letters = ["c", "f", "j"]
+// target = "c"
+// Output: "f"
+
+// Input:
+// letters = ["c", "f", "j"]
+// target = "d"
+// Output: "f"
+
+// Input:
+// letters = ["c", "f", "j"]
+// target = "g"
+// Output: "j"
+
+// first pass linear search
+var nextGreatestLetter = function(letters, target) {
+    for (let char of letters) {
+        if (char > target) {
+            return char;
+        }
+    } 
+    return letters[0];
+};
+
