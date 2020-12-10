d6b46469a1f47b90c5d96fae943898892c9bf17e
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2020-12-10 11:15:15 -0800

    Update 162-findPeakElement.js

--- leetcode/medium/162-findPeakElement.js
+++ leetcode/medium/162-findPeakElement.js
@@ -15,7 +15,7 @@
 // Output: 5
 // Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
 
-// first pass solution
+// first pass linear search solution
 var findPeakElement = function(nums) {
     if (nums.length === 1) return 0;
     
@@ -30,4 +30,23 @@ var findPeakElement = function(nums) {
         }
     }
     return -1;
+};
+
+// second pass solution using binary search
+var findPeakElement = function(nums) {
+    let low = 0;
+    let high = nums.length - 1;
+    
+    while (low <  high) {
+        let mid = Math.floor((low + high) / 2);
+        
+        if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+            return mid;
+        } else if (nums[mid] < nums[mid + 1]){
+            low = mid + 1;
+        } else if (nums[mid] > nums[mid + 1]){
+            high = mid - 1;
+        }
+    }
+    return low;
 };
