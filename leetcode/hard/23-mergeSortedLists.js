25f65efb14cb34048cc5c1e46babfb887a1c6636
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-01-08 08:55:08 -0800

    Update 23-mergeSortedLists.js

--- leetcode/hard/23-mergeSortedLists.js
+++ leetcode/hard/23-mergeSortedLists.js
@@ -15,6 +15,7 @@
 // merging them into one sorted list:
 // 1->1->2->3->4->4->5->6
 
+// first pass solution
 var mergeKLists = function(lists) {    
     let arr = [];
     
@@ -36,6 +37,39 @@ var mergeKLists = function(lists) {
         curr.next = new ListNode(arr[i]);
         curr = curr.next;
     }
-    
     return head;
 };
+
+// second pass solution
+var mergeKLists = function(lists) {
+    if (!lists.length) return null;
+    
+    while (lists.length > 1) {
+        let list1 = lists.pop();
+        let list2 = lists.pop();
+        let sortedList = merge(list1, list2);
+        lists.push(sortedList);
+    }
+    return lists.pop();
+};
+
+var merge = function(list1, list2) {
+    let head = new ListNode(0);
+    let headRef = head;
+
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            head.next = new ListNode(list1.val);
+            list1 = list1.next;
+        } else {
+            head.next = new ListNode(list2.val);
+            list2 = list2.next;
+        }
+        head = head.next;
+    }
+
+    if (list1) head.next = list1;
+    else head.next = list2;
+
+    return headRef.next;
+}
