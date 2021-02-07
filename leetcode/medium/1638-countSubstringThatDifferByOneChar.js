0ab32bf0d7da5fa9c0bb60bd4d527398228f2913
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-02-07 13:56:19 -0800

    Create 1638-countSubstringThatDifferByOneChar.js

---
+++ leetcode/medium/1638-countSubstringThatDifferByOneChar.js
@@ -0,0 +1,50 @@
+/*
+Given two strings s and t, find the number of ways you can choose a non-empty substring of s and replace a single character by a 
+different character such that the resulting substring is a substring of t. In other words, find the number of substrings in s that differ from some substring in t by exactly one character.
+
+For example, the underlined substrings in "computer" and "computation" only differ by the 'e'/'a', so this is a valid way.
+
+Return the number of substrings that satisfy the condition above.
+
+A substring is a contiguous sequence of characters within a string.
+
+Example 1:
+Input: s = "aba", t = "baba"
+Output: 6
+Explanation: The following are the pairs of substrings from s and t that differ by exactly 1 character:
+("aba", "baba")
+("aba", "baba")
+("aba", "baba")
+("aba", "baba")
+("aba", "baba")
+("aba", "baba")
+The underlined portions are the substrings that are chosen from s and t.
+
+Example 2:
+Input: s = "ab", t = "bb"
+Output: 3
+Explanation: The following are the pairs of substrings from s and t that differ by 1 character:
+("ab", "bb")
+("ab", "bb")
+("ab", "bb")
+The underlined portions are the substrings that are chosen from s and t.
+*/
+
+var countSubstrings = function(s, t) {
+    let output = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        for (let j = 0; j < t.length; j++) {
+            let k = i;
+            let m = j;
+            let count = 0;
+            while (k < s.length && m < t.length) {
+                if (s[k] !== t[m]) count++;
+                if (count === 1) output++;
+                else if (count > 1) break;
+                k++, m++;
+            }
+        }
+    }
+    return output;
+};
