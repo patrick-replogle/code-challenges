b20a377b0ddad8fcca3eae42f0887420c8d9cbe9
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-02-08 15:14:17 -0800

    Create 1670-designFrontMiddleBackQueue.js

---
+++ leetcode/medium/1670-designFrontMiddleBackQueue.js
@@ -0,0 +1,74 @@
+/*
+Design a queue that supports push and pop operations in the front, middle, and back.
+
+Implement the FrontMiddleBack class:
+
+FrontMiddleBack() Initializes the queue.
+void pushFront(int val) Adds val to the front of the queue.
+void pushMiddle(int val) Adds val to the middle of the queue.
+void pushBack(int val) Adds val to the back of the queue.
+int popFront() Removes the front element of the queue and returns it. If the queue is empty, return -1.
+int popMiddle() Removes the middle element of the queue and returns it. If the queue is empty, return -1.
+int popBack() Removes the back element of the queue and returns it. If the queue is empty, return -1.
+Notice that when there are two middle position choices, the operation is performed on the frontmost middle position choice. For example:
+
+Pushing 6 into the middle of [1, 2, 3, 4, 5] results in [1, 2, 6, 3, 4, 5].
+Popping the middle from [1, 2, 3, 4, 5, 6] returns 3 and results in [1, 2, 4, 5, 6].
+ 
+
+Example 1:
+Input:
+["FrontMiddleBackQueue", "pushFront", "pushBack", "pushMiddle", "pushMiddle", "popFront", "popMiddle", "popMiddle", "popBack", "popFront"]
+[[], [1], [2], [3], [4], [], [], [], [], []]
+Output:
+[null, null, null, null, null, 1, 3, 4, 2, -1]
+*/
+
+// first pass solution
+class FrontMiddleBackQueue {
+    constructor() {
+        this.queue = []; // using a linked list would be more efficient
+        this.len = 0;
+    }
+    pushFront(val) {
+        this.queue.unshift(val);
+        this.len++;
+    }
+    pushMiddle(val) {
+        if (this.len <= 1) {
+            this.pushFront(val);
+        }
+        else {
+            let mid = Math.floor(this.len / 2);
+            this.queue.splice(mid, 0, val);
+            this.len++;
+        }
+    }
+    pushBack(val) {
+        this.queue.push(val);
+        this.len++;
+    }
+    popFront() {
+        if (this.len) {
+            this.len--;
+            return this.queue.shift();
+        }
+        return -1;
+    }
+    popMiddle() {
+        if (this.len) {
+            let mid = Math.floor(this.len / 2);
+            if (this.len % 2 === 0) mid -= 1;
+            this.len--;
+            return this.queue.splice(mid, 1).pop();
+        }
+        return -1;
+    }
+    popBack() {
+        if (this.len) {
+            this.len--;
+            return this.queue.pop();
+        }
+        return -1;
+    }
+}
