3f19d01cddb564b50a8a8a663632f7974f2fbce3
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-02-09 11:14:52 -0800

    Create 1710-maximumUnitsOnATruck.js

---
+++ leetcode/easy/1710-maximumUnitsOnATruck.js
@@ -0,0 +1,44 @@
+/*
+You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes, where boxTypes[i] = [numberOfBoxesi, 
+numberOfUnitsPerBoxi]:
+
+numberOfBoxesi is the number of boxes of type i.
+numberOfUnitsPerBoxi is the number of units in each box of the type i.
+You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck. You can choose 
+any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+
+Return the maximum total number of units that can be put on the truck.
+
+Example 1:
+Input: boxTypes = [[1,3],[2,2],[3,1]], truckSize = 4
+Output: 8
+Explanation: There are:
+- 1 box of the first type that contains 3 units.
+- 2 boxes of the second type that contain 2 units each.
+- 3 boxes of the third type that contain 1 unit each.
+You can take all the boxes of the first and second types, and one box of the third type.
+The total number of units will be = (1 * 3) + (2 * 2) + (1 * 1) = 8.
+*/
+
+// first pass solution
+var maximumUnits = function(boxTypes, truckSize) {
+    let output = 0;
+    boxTypes.sort((a, b) => b[1] - a[1]);
+    
+    for (let type of boxTypes) {
+        let [quantity, units] = type;
+        
+        if (truckSize >= quantity) {
+            output += quantity * units;
+            truckSize -= quantity;
+        } else {
+            while (truckSize > 0 && quantity > 0) {
+                output += units;
+                truckSize--;
+                quantity--;
+            }
+        }
+        if (truckSize === 0) break;
+    }
+    return output;
+};
