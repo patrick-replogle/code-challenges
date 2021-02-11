bcd7b5a39066612081c29c754f3da5961c76752a
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-02-11 08:28:59 -0800

    Create 196-deleteDuplicateEmails.sql

---
+++ leetcode/easy/196-deleteDuplicateEmails.sql
@@ -0,0 +1,26 @@
+/*
+Write a SQL query to delete all duplicate email entries in a table named Person, keeping only unique emails based on its smallest Id.
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Id is the primary key column for this table.
+For example, after running your query, the above Person table should have the following rows:
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+*/
+
+# first pass solution
+DELETE p2
+FROM Person p1, Person p2
+WHERE p1.email = p2.email
+AND p2.Id > p1.Id;
