46aaf99b3878c89189f62536cd6b51707497a94c
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-02-11 15:26:16 -0800

    Create 236-lowestCommonAncestorOfBinaryTree.js

---
+++ leetcode/medium/236-lowestCommonAncestorOfBinaryTree.js
@@ -0,0 +1,24 @@
+/*
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest 
+node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+Example 1:
+Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+Output: 3
+Explanation: The LCA of nodes 5 and 1 is 3.
+*/
+
+var lowestCommonAncestor = function(root, p, q) {
+    if (!root) return null;
+    
+    if (root.val === p.val || root.val === q.val) {
+        return root;
+    }
+    let left = lowestCommonAncestor(root.left, p, q);
+    let right = lowestCommonAncestor(root.right, p, q);
+    
+    if (left && right) return root;
+    return left || right;
+}
