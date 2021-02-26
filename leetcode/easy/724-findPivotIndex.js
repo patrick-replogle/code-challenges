844daeeb1ca73089a2c75aaff00d20082c61eb43
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-02-26 15:49:16 -0800

    Create 724-findPivotIndex.js

---
+++ leetcode/easy/724-findPivotIndex.js
@@ -0,0 +1,46 @@
+/*
+Given an array of integers nums, calculate the pivot index of this array.
+
+The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the 
+numbers strictly to the index's right.
+
+If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies 
+to the right edge of the array.
+
+Return the leftmost pivot index. If no such index exists, return -1.
+
+Example 1:
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11
+
+Example 2:
+Input: nums = [1,2,3]
+Output: -1
+Explanation:
+There is no index that satisfies the conditions in the problem statement.
+*/
+
+// first pass solution
+var pivotIndex = function(nums) {
+    let leftSum = new Array(nums.length).fill(0);
+    let rightSum = new Array(nums.length).fill(0);
+    
+    for (let i = 1; i < nums.length; i++) {
+        leftSum[i] = nums[i-1] + leftSum[i-1];
+    }
+    
+    for (let i = nums.length - 2; i >= 0; i--) {
+        rightSum[i] = nums[i+1] + rightSum[i+1];
+    }
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (rightSum[i] === leftSum[i]) {
+            return i;
+        }
+    }
+    return -1;
+};
