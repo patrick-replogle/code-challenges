0ce2659c491cdfc64f600a1d60e219233a433160
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-03-17 09:31:09 -0700

    Create combinationLock.sql

---
+++ codesignal/combinationLock.sql
@@ -0,0 +1,33 @@
+/*
+You returned from vacation with a huge suitcase full of presents. Unfortunately, you forgot the correct combination for the combination 
+lock on the bag, and now you have to try all of them until you find the correct one. You're curious about how many possible combinations 
+the lock has.
+
+The combination lock consists of several rotating discs, where each disc has its own set of possible characters. You have a table discs 
+which stores the information about these discs and has the following structure:
+
+id: the unique ID of a disc;
+characters: the list of characters the disc has (the characters are guaranteed to be unique);
+color: the color of the disc.
+Calculate the total number of all possible combinations that the lock has, and return it as a table that has only one column combinations 
+and one row.
+
+Example
+
+For the following table discs
+
+id	characters	color
+1	code	blue
+2	fights	white
+the output should be
+
+combinations
+24
+*/
+
+# first pass solution
+CREATE PROCEDURE combinationLock()
+BEGIN
+    SELECT ROUND(EXP(SUM(LOG(LENGTH(characters))))) AS combinations 
+    FROM discs;
+END
