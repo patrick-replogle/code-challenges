89f29800d5b8757f5231bf77be0d2fba5d31f098
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-04-02 11:41:36 -0700

    Create findLongestSubarrayBySum.js

---
+++ codesignal/findLongestSubarrayBySum.js
@@ -0,0 +1,76 @@
+/*
+You have an unsorted array arr of non-negative integers and a number s. Find a longest contiguous subarray in arr that has a sum equal to s. 
+Return two integers that represent its inclusive bounds. If there are several possible answers, return the one with the smallest left bound. 
+If there are no answers, return [-1].
+
+Your answer should be 1-based, meaning that the first position of the array is 1 instead of 0.
+
+Example
+
+For s = 12 and arr = [1, 2, 3, 7, 5], the output should be
+findLongestSubarrayBySum(s, arr) = [2, 4].
+
+The sum of elements from the 2nd position to the 4th position (1-based) is equal to 12: 2 + 3 + 7.
+
+For s = 15 and arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], the output should be
+findLongestSubarrayBySum(s, arr) = [1, 5].
+
+The sum of elements from the 1st position to the 5th position (1-based) is equal to 15: 1 + 2 + 3 + 4 + 5.
+
+For s = 15 and arr = [1, 2, 3, 4, 5, 0, 0, 0, 6, 7, 8, 9, 10], the output should be
+findLongestSubarrayBySum(s, arr) = [1, 8].
+
+The sum of elements from the 1st position to the 8th position (1-based) is equal to 15: 1 + 2 + 3 + 4 + 5 + 0 + 0 + 0.
+*/
+
+// first pass solution => times out
+function findLongestSubarrayBySum(s, arr) {   
+    let longest = 0;
+    let output = null;
+    
+    for (let i = 0; i < arr.length; i++) {
+        let currSum = 0;
+        for (let j = i; j < arr.length; j++) {
+            currSum += arr[j];
+            if (currSum === s) {
+                if (j - i + 1 > longest) {
+                    longest = j - i;
+                    output = [i+1, j+1];
+                }  
+            } 
+            if (currSum > s) break;
+        }
+    }
+    return output ? output : [-1];
+}
+
+// second pass solution
+function findLongestSubarrayBySum(s, arr) {   
+    let left = 0;
+    let right = 0;
+    let longest = 0;
+    let output = null;
+    let curr = 0;
+    
+    while (right < arr.length) {
+        curr += arr[right];
+        if (curr === s) {
+            if (right - left + 1 > longest) {
+                longest = right - left;
+                output = [left + 1, right + 1]; 
+            }
+        }
+        while (curr > s) {
+            curr -= arr[left];
+            left++;
+            if (curr === s) {
+                if (right - left + 1 > longest) {
+                    longest = right - left;
+                    output = [left + 1, right + 1]; 
+                }
+            }
+        }
+        right++;
+    }
+    return output ? output : [-1];
+}
