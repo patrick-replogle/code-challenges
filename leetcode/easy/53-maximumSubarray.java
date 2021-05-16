57c0dce1e82eebf31bb4c4168e584ade8e222e36
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-05-16 13:31:00 -0700

    Create 53-maximumSubarray.java

---
+++ leetcode/easy/53-maximumSubarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        int output = nums[0];
+        dp[0] = nums[0];
+        
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
+            output = Math.max(output, dp[i]);
+        }
+        return output;
+    }
+}
