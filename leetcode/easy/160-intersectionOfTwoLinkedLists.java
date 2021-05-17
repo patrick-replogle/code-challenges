f2feabc298d5d47ede2baa222b72933725b1e3d7
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-05-17 13:50:22 -0700

    Create 160-intersectionOfTwoLinkedLists.java

---
+++ leetcode/easy/160-intersectionOfTwoLinkedLists.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        
+        HashSet<ListNode> set = new HashSet<>();
+        
+        while (headA != null) {
+            set.add(headA);
+            headA = headA.next;
+        }
+        
+        while (headB != null) {
+            if (set.contains(headB)) {
+                return headB;
+            }
+            headB = headB.next;
+        }
+        return null;
+    }
+}
