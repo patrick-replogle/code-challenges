aa55f785cd52604aedcc301ca47917a2be6d65e1
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-05-24 11:31:30 -0700

    Create 190-reverseBits.java

---
+++ leetcode/easy/190-reverseBits.java
@@ -0,0 +1,16 @@
+// first pass solution
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int output = 0;
+
+        for (int i = 0; i < 32; i++) {
+            output <<= 1;
+            if ((n & 1) > 0) {
+                output |= (n & 1);
+            }
+            n >>= 1;
+        }
+        return output;
+    }
+}
