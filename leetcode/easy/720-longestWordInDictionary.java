1dc524ca823c288e8c57822afc0e41ebe5b5d162
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-05-30 05:16:55 -0700

    Update 720-longestWordInDictionary.java

--- leetcode/easy/720-longestWordInDictionary.java
+++ leetcode/easy/720-longestWordInDictionary.java
@@ -1,3 +1,4 @@
+// first pass solution
 class Solution {
     public String longestWord(String[] words) {
         HashSet<String> set = new HashSet<>();
@@ -24,5 +25,34 @@ class Solution {
             }
         }
         return output;
+    }
+}
+
+// slightly more optimized
+class Solution {
+    public String longestWord(String[] words) {
+        HashSet<String> set = new HashSet<>();
+        String output = "";
+        Arrays.sort(words, (w1, w2) -> w1.length() == w2.length() ? 
+                    w1.compareTo(w2) : w2.length() - w1.length());
+        
+        for (String w : words) set.add(w);
+
+        for (String s : words) {
+            String sub = "";
+            
+            for (int i = 0; i < s.length(); i++) {
+                String currSub = s.substring(0, i + 1);
+                if (set.contains(currSub)) {
+                    sub = currSub;
+                } else {
+                    break;
+                }
+            }
+            if (sub.length() == s.length()) {
+                return s;
+            } 
+        }
+        return "";
     }
 }
