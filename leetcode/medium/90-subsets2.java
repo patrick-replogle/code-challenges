39fc0b96cf17f24c1e805a32584fe9d2800d3680
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-06-03 08:33:14 -0700

    Update 90-subsets2.java

--- leetcode/medium/90-subsets2.java
+++ leetcode/medium/90-subsets2.java
@@ -32,3 +32,33 @@ class Solution {
         }
     }
 }
+
+// slightly more optimized
+class Solution {
+    List<List<Integer>> output = new ArrayList<>();
+    
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<Integer> subset = new ArrayList<>();
+        Arrays.sort(nums);
+        combos(subset, nums, 0);
+  
+        return output;
+    }
+    
+    public void combos(List<Integer> subset, int[] nums, int index) {
+        List<Integer> subsetCopy = new ArrayList<>(subset);
+        HashSet<Integer> seen = new HashSet<>();
+        
+        output.add(subsetCopy);
+        
+        for(int i = index; i < nums.length; i++) {
+            if (!seen.contains(nums[i])) {
+                subset.add(nums[i]);
+                combos(subset, nums, i + 1);
+                subset.remove(subset.size() - 1);
+                seen.add(nums[i]);
+            }
+        }
+    }
+}
+
