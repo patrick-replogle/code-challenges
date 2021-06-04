ea627170e8f2a27f2fe18afd5c10f89a0634fd7c
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-06-03 17:36:09 -0700

    Create 40-combinationSum2.java

---
+++ leetcode/medium/40-combinationSum2.java
@@ -0,0 +1,29 @@
+// first pass solution
+class Solution {
+    List<List<Integer>> output = new ArrayList<>();
+    
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        
+        recurse(target, candidates, 0, new ArrayList<Integer>());
+        
+        return output;
+    }
+    
+    public void recurse(int target, int[] nums, int index, List<Integer> path) {
+        if (target == 0) {
+            if (!output.contains(path)) {
+                output.add(path);
+                return;
+            }
+        }
+        
+        for (int i = index; i < nums.length; i++) {
+            if (target - nums[i] >= 0) {
+                List<Integer> copy = new ArrayList<>(path);
+                copy.add(nums[i]);
+                recurse(target - nums[i], nums, i + 1, copy);
+            }
+        }
+     }
+}
