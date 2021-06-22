e4eacb2eb5373568028ae1c57c953a6e7501126b
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-06-21 21:53:07 -0700

    Create 701-insertIntoBinarySearchTree.java

---
+++ leetcode/medium/701-insertIntoBinarySearchTree.java
@@ -0,0 +1,23 @@
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        
+        if (val < root.val) {
+            if (root.left == null) {
+                root.left = new TreeNode(val);
+            } else {
+                insertIntoBST(root.left, val);
+            }
+        }
+        else if (val > root.val) {
+            if (root.right == null) {
+                root.right = new TreeNode(val);
+            } else {
+                insertIntoBST(root.right, val);
+            }
+        } 
+        return root;
+    }
+}
