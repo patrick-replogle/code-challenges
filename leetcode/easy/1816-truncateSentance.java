043615cd945ecce92d188556868b2b3b5f293019
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-07-24 16:43:28 -0700

    Create 1816-truncateSentance.java

---
+++ leetcode/easy/1816-truncateSentance.java
@@ -0,0 +1,26 @@
+// first pass solution
+class Solution {
+    public String truncateSentence(String s, int k) {
+        List<String> arr = new ArrayList<>(Arrays.asList(s.split(" ")));
+        
+        while (arr.size() > k) {
+            arr.remove(arr.size() - 1);
+        }
+        
+        return String.join(" ", arr);
+    }
+}
+
+// second pass solution
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String[] strArr = s.split(" ");
+        String output = "";
+        
+        for (int i = 0; i < k; i++) {
+            output += strArr[i] + " ";
+        }
+        
+        return output.trim();
+    }
+}
