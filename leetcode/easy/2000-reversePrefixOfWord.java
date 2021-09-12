48444b23ee59f73260b528049c8ee4356ce556a2
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-09-12 16:48:33 -0700

    Create 2000-reversePrefixOfWord.java

---
+++ leetcode/easy/2000-reversePrefixOfWord.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        StringBuilder sb = new StringBuilder();
+        int index = word.indexOf(ch);
+        
+        for (int i = index; i >= 0; i--) {
+            sb.append(word.charAt(i));
+        }
+        
+        for (int i = index + 1; i < word.length(); i++) {
+            sb.append(word.charAt(i));
+        }
+        
+        return sb.toString();
+    }
+}
