e67a30b5e691bb476704bd7fd07b69095c7302dc
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-09-22 10:18:03 -0700

    Create 2006-countPairsWithAbsoluteDiffK.java

---
+++ leetcode/easy/2006-countPairsWithAbsoluteDiffK.java
@@ -0,0 +1,15 @@
+// first pass solution
+class Solution {
+    public int countKDifference(int[] nums, int k) {
+        int pairCount = 0;
+        
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (Math.abs(nums[i] - nums[j]) == k) {
+                    pairCount++;
+                }
+            }
+        }
+        return pairCount;
+    }
+}
