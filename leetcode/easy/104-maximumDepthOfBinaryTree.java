b90ac02ab7082d01022be49277210781b8b2783e
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-10-04 09:05:05 -0700

    Create 104-maximumDepthOfBinaryTree.java

---
+++ leetcode/easy/104-maximumDepthOfBinaryTree.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        int left = maxDepth(root.left) + 1;
+        int right = maxDepth(root.right) + 1;
+        
+        return Math.max(left, right);
+    }
+}
