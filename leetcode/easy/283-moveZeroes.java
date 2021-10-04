72173ba89ac554111975a05b75fc89088a71c454
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-10-04 09:25:28 -0700

    Update 283-moveZeroes.java

--- leetcode/easy/283-moveZeroes.java
+++ leetcode/easy/283-moveZeroes.java
@@ -16,3 +16,20 @@ class Solution {
     }
 }
 
+// second pass solution
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        int right = 0;
+        
+        while (right < nums.length) {
+            if (nums[left] == 0) {
+                nums[left] = nums[right];
+                nums[right] = 0;
+            }
+            if (nums[left] != 0) left++;
+            right++;
+        }
+    }
+}
+
