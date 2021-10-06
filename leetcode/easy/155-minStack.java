09a3755581e37e0575d06fc895ed287da08df429
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2021-10-05 18:30:04 -0700

    Update 155-minStack.java

--- leetcode/easy/155-minStack.java
+++ leetcode/easy/155-minStack.java
@@ -1,3 +1,4 @@
+// solution 1
 class MinStack {
     private ArrayList<Integer> stack;
     private ArrayList<Integer> min;
@@ -32,3 +33,50 @@ class MinStack {
         return min.get(min.size() - 1);
     }
 }
+
+// solution 2
+class StackEntry {
+    int min;
+    int value;
+    
+    public StackEntry(int min, int value) {
+        this.min = min;
+        this.value = value;
+    }
+}
+
+class MinStack {
+    Stack<StackEntry> stack;
+    
+    public MinStack() {
+        this.stack = new Stack();
+    }
+    
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            stack.add(new StackEntry(val, val));
+        } else {
+            int currMin = stack.peek().min < val ? stack.peek().min : val;
+            stack.add(new StackEntry(currMin, val)); 
+        }
+    }
+    
+    public void pop() {
+        if (!stack.isEmpty()) stack.pop();
+    }
+    
+    public int top() {
+        if (!stack.isEmpty()) {
+            return stack.peek().value;
+        }
+        return -1;
+    }
+    
+    public int getMin() {
+        if (!stack.isEmpty()) {
+            return stack.peek().min;
+        }
+        return -1;
+    }
+}
+
