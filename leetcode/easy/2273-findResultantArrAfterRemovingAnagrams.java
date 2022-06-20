9df53786ce080aba73927e22aff45a446c5e8e20
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2022-06-20 14:58:40 -0700

    Update 2273-findResultantArrAfterRemovingAnagrams.java

--- leetcode/easy/2273-findResultantArrAfterRemovingAnagrams.java
+++ leetcode/easy/2273-findResultantArrAfterRemovingAnagrams.java
@@ -1,3 +1,4 @@
+// first pass solution
 class Solution {
     public List<String> removeAnagrams(String[] words) {
         if (words.length == 0) return new ArrayList<String>();
@@ -21,6 +22,42 @@ class Solution {
             }
          }
         
+        return output;
+    }
+}
+
+// second pass solution
+class Solution {
+    public List<String> removeAnagrams(String[] words) {
+        if (words.length == 0) return new ArrayList<String>();
+        
+        List<String> output = new ArrayList<>();
+        output.add(words[0]);
+
+        for (int i = 1; i < words.length; i++) {
+            if (words[i].length() != words[i-1].length()) {
+                output.add(words[i]);
+                continue;
+            }
+            
+            int[] arr1 = new int[26];
+            int[] arr2 = new int[26];
+            
+            for (int j = 0; j < words[i].length(); j++) {
+                int charCode1 = (int) words[i].charAt(j) - 97;
+                int charCode2 = (int) words[i-1].charAt(j) - 97;
+                
+                arr1[charCode1]++;
+                arr2[charCode2]++;
+            }
+            
+            for (int j = 0; j < 26; j++) {
+                if (arr1[j] != arr2[j]) {
+                    output.add(words[i]);
+                    break;
+                }
+            }
+         }
         return output;
     }
 }
