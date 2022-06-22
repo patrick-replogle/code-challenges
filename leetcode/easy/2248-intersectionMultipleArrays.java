c1016bb769b3d2bed88a3c1e3149eeeeb5b1725f
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2022-06-22 10:39:27 -0700

    Update 2248-intersectionMultipleArrays.java

--- leetcode/easy/2248-intersectionMultipleArrays.java
+++ leetcode/easy/2248-intersectionMultipleArrays.java
@@ -1,17 +1,16 @@
 class Solution {
     public List<Integer> intersection(int[][] nums) {
         List<Integer> output = new ArrayList<>();
-        HashMap<Integer, Set<Integer>> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         
         for (int i = 0; i < nums.length; i++) {
             for (int num : nums[i]) {
-                if (!map.containsKey(num)) map.put(num, new HashSet<>());
-                map.get(num).add(i);
+                map.put(num, map.getOrDefault(num, 0) + 1);
             }
         }
         
-        for (Map.Entry<Integer, Set<Integer>> entry : map.entrySet()) {
-            if (entry.getValue().size() == nums.length) {
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == nums.length) {
                 output.add(entry.getKey());
             }
         }
