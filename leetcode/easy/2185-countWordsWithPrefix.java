82aa5cfc1ea6a679612a32899255ac92e77f2530
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2022-06-27 16:17:56 -0700

    Create 2185-countWordsWithPrefix.java

---
+++ leetcode/easy/2185-countWordsWithPrefix.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count = 0;
+        
+        for (String s: words) {
+            if (s.startsWith(pref)) count++;
+        }
+  
+        return count;
+    }
+}
