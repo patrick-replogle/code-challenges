08d69b5a9f4b43fc02c60f67e4dc3a7f7eb4c27f
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2022-07-13 08:18:37 -0700

    Create 2331-evaluateBooleanBinaryTree.java

---
+++ leetcode/easy/2331-evaluateBooleanBinaryTree.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if (root == null || root.val == 0) return false;
+        if (root.val == 1) return true;
+        if (root.val == 2) return evaluateTree(root.left) || evaluateTree(root.right);
+
+        return evaluateTree(root.left) && evaluateTree(root.right);
+    }
+}
