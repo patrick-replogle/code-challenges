4bf2a9f46681d5ff345bbc60f2cd844662a85ccf
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2022-11-29 15:52:10 -0800

    Create 2475-numberOfUnequalTripletsInArray.java

---
+++ leetcode/easy/2475-numberOfUnequalTripletsInArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int unequalTriplets(int[] nums) {
+        int output = 0;
+        
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k]) {
+                        output++;
+                    }
+                }
+            }
+        }
+        return output;
+    }
+}
