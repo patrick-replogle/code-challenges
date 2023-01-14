48ef5207eb514997e683a18fde7c19d5947ab138
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2023-01-14 14:10:24 -0800

    Create 125-validPalindrome.cpp

---
+++ leetcode/easy/125-validPalindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i <= j) {
+            while (i < j && i <= j && !iswalnum(s[i])) i++;
+            while (j >= 0 && j >= i && !iswalnum(s[j])) j--;
+            if (i > j) return true;
+            if (tolower(s[i]) != tolower(s[j])) return false;
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};
