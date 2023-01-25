81e87ca64f6ec857789e1175a56253b29fb11a6f
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2023-01-25 09:12:55 -0800

    Create 203-removeLinkedListElements.cpp

---
+++ leetcode/easy/203-removeLinkedListElements.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        while (head != nullptr && head->val == val) {
+            head = head->next;
+        }
+
+        ListNode* curr = head;
+        ListNode* prev = head;
+
+        while (curr != nullptr) {
+            if (curr->val == val) {
+                prev->next = curr->next;
+            } else {
+                prev = curr;
+            }
+            curr = curr->next;
+        }
+
+        return head;
+    }
+};
