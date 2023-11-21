8451c9b08dcc1365697c267ac28afef2d916eff0
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2023-11-21 14:05:14 -0800

    Update 70-climbing Stairs.cpp

--- leetcode/easy/70-climbing Stairs.cpp
+++ leetcode/easy/70-climbing Stairs.cpp
@@ -1,3 +1,4 @@
+// top down
 class Solution {
 public:
     int climbStairs(int n) {
@@ -16,3 +17,24 @@ public:
         return m[n];
     }
 };
+
+// bottom up
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int prevPrev = 1;
+        int prev = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int next = prevPrev + prev;
+            prevPrev = prev;
+            prev = next;
+        }
+
+        return prev;
+    } 
+};
+
