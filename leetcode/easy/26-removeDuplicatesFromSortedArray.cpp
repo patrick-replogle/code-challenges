663af9f7bf1bc2251bd4170fbe5469eefc8d9186
Patrick Replogle <50844285+patrick-replogle@users.noreply.github.com>
2023-12-06 17:25:47 -0800

    Update 26-removeDuplicatesFromSortedArray.cpp

--- leetcode/easy/26-removeDuplicatesFromSortedArray.cpp
+++ leetcode/easy/26-removeDuplicatesFromSortedArray.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int> &nums) {
-        if (nums.size() <= 1) return nums.size();
-        
-        int i = 1;
+    int removeDuplicates(vector<int>& nums) {
+      int left = 1;
 
-        for (; i < nums.size(); i++) {
-            if (nums[i] <= nums[i-1]) {
-                int j = i + 1;
+      for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] > nums[left - 1]) {
+          nums[left] = nums[i];
+          left++;
+        } 
+      }
 
-                while (j < nums.size() && nums[j] <= nums[i-1]) j++;
-              
-                if (j >= nums.size() || nums[j] < nums[i]) break;
-              
-                swap(nums[i], nums[j]);
-            }
-        }
-
-        return i;
+      return left;
     }
 };
